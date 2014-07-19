bc9173b3ad3817bb5ed49098c97d9b523bdfaebb
Richard Mortier <mort@cantab.net>
2014-07-19 15:43:28 -0700

    [theme] update `init.js` for reveal.js-2.6.2

--- assets/reveal.js-2.6.2/js/init.js
+++ assets/reveal.js-2.6.2/js/init.js
@@ -1,19 +1,12 @@
-// Full list of configuration options available here:
-// https://github.com/hakimel/reveal.js#configuration
 Reveal.initialize({
-
-/*
-    controls: true,
-    progress: true,
+    center: false,
     history: true,
-    center: true,
-    margin:0.07,
+    slideNumber: true,
+    theme: Reveal.getQueryHash().theme,
+    transition: Reveal.getQueryHash().transition || "fade",
 
-    theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
-    transition: Reveal.getQueryHash().transition || "fade", // default/cube/page/concave/zoom/linear/fade/none
-*/
-    // Optional libraries used to extend on reveal.js
     dependencies: [
+        // cross-browser classlist shim
         { src: "/reveal.js-2.6.2/lib/js/classList.js",
           condition: function()
           {
@@ -21,13 +14,13 @@ Reveal.initialize({
           }
         },
 
+        // markdown slide support
         { src: "/reveal.js-2.6.2/plugin/markdown/marked.js",
           condition: function()
           {
               return !!document.querySelector( "[data-markdown]" );
           }
         },
-
         { src: "/reveal.js-2.6.2/plugin/markdown/markdown.js",
           condition: function()
           {
@@ -35,30 +28,9 @@ Reveal.initialize({
           }
         },
 
+        // syntax highlight <code> elements
         { src: "/reveal.js-2.6.2/plugin/highlight/highlight.js", async: true,
           callback: function() { hljs.initHighlightingOnLoad(); } },
-
-        { src: "/reveal.js-2.6.2/plugin/zoom-js/zoom.js", async: true,
-          condition: function() { return !!document.body.classList; } },
-
-        { src: "/reveal.js-2.6.2/plugin/notes/notes.js", async: true,
-          condition: function() { return !!document.body.classList; } }
-
-        // { src: "/reveal.js-2.6.2/plugin/search/search.js", async: true, condition: function() { return !!document.body.classList; } }
-        // { src: "/reveal.js-2.6.2/plugin/remotes/remotes.js", async: true, condition: function() { return !!document.body.classList; } }
     ]
 
 });
-
-/*
-var slideNumber = document.querySelector("#slide-number");
-var updateSlideNumber = function(event) {
-    slideNumber.textContent =
-        (event.indexh > 0)
-        ? event.indexh + "\u2014" + (event.indexv +1)
-        : "";
-}
-
-Reveal.addEventListener("ready", updateSlideNumber);
-Reveal.addEventListener("slidechanged", updateSlideNumber);
-*/
