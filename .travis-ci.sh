e4c3ca22b190069fad368c2320c5545a4eb7d233
Anil Madhavapeddy <anil@recoil.org>
2014-09-03 15:43:34 +0200

    init from git

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -22,7 +22,7 @@ echo OPAM versions
 opam --version
 opam --git-version
 
-opam init
+opam init git://github.com/ocaml/opam-repository >/dev/null 2>&1
 opam remote add mirage-dev git://github.com/mirage/mirage-dev
 opam update
 opam install ${OPAM_PACKAGES}
