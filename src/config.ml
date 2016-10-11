cf66d4c35b5a9bcd17136846da086c90ac128e71
Richard Mortier <mort@cantab.net>
2016-10-11 13:21:57 +0100

    src: add library deps and `port` key to config

--- src/config.ml
+++ src/config.ml
@@ -1,5 +1,14 @@
 open Mirage
 
+let port =
+  let doc = Key.Arg.info
+      ~doc:"Listening port."
+      ~docv:"PORT" ~env:"PORT" ["port"]
+  in
+  Key.(create "port" Arg.(opt int 80 doc))
+
+let keys = Key.([ abstract port ])
+
 let fs_key = Key.(value @@ kv_ro ())
 let assetsfs = generic_kv_ro ~key:fs_key "../assets"
 let slidesfs = generic_kv_ro ~key:fs_key "../slides"
@@ -7,10 +16,11 @@ let slidesfs = generic_kv_ro ~key:fs_key "../slides"
 let stackv4 = generic_stackv4 default_console tap0
 let httpsvr = http_server @@ conduit_direct ~tls:false stackv4
 
-let libraries = [ "tyxml" ]
-let packages = [ "tyxml" ]
+let libraries = [ "tyxml"; "mirage-http"; "logs"; "mirage-logs"; "magic-mime" ]
+let packages = [ "tyxml"; "mirage-http"; "logs"; "mirage-logs"; "magic-mime" ]
+
 let http_job =
-  foreign ~libraries ~packages "Server.Main"
+  foreign ~libraries ~packages ~keys "Server.Main"
     (clock @-> http @-> kv_ro @-> kv_ro @-> job)
 
 let () =
